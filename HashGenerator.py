7184fdb88ad902abb372c5760674ca5268911a45
zootboy <zootboysean@gmail.com>
2015-08-12 22:05:35 -0400

    Updated to python3 and added salt generation.

--- HashGenerator.py
+++ HashGenerator.py
@@ -1,12 +1,14 @@
-#!/usr/bin/python
-import re, hashlib
+#!/usr/bin/python3
+import re, hashlib, random, string
 
 def main():
     while True:
-        raw = raw_input('Swipe ID card:\n')
+        raw = input('Swipe ID card:\n')
         uid = re.search('[0-9]+',raw).group(0)
-        idhash = hashlib.sha256(uid).hexdigest()
-        print 'SHA-256 hash:\n', idhash
+        salt = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
+        instring = salt + uid
+        idhash = hashlib.sha256(instring.encode("UTF-8")).hexdigest()
+        print('Salt+hash:', salt + "+" + idhash)
 
 if __name__ == "__main__":
     main()
